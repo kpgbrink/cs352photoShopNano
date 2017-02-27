92afa1ad89154d081a5980d117875c271aa3c5a6
Krisofer Brink <kpgbrink@gmail.com>
2017-02-27 00:46:10 -0500

    Finish photoshop

--- CanvasPaint.js
+++ CanvasPaint.js
@@ -10,7 +10,6 @@ $(document).ready(function () { cpaint.init(); });
 var cpaint = {
   drawing: 		false,
   tool:			'marker',
-  lineThickness: 	12,
   color:		'#333399',
 }
 
@@ -36,7 +35,7 @@ cpaint.init = function () {
   $('#color1').bind('change', cpaint.colorChange);
   $('#color1').colorPicker();			// initialize color picker
   $('#mainmenu').clickMenu();			// initialize menu
-  $('#widthSlider').bind('')
+    
 
   // bind menu options
   $('#menuClear').bind('click', cpaint.clear);
@@ -48,8 +47,100 @@ cpaint.init = function () {
   $('#menuSave').bind('click',cpaint.save);
   $('#toolBar').show();		// when toolbar is initialized, make it visible
     
-  cpaint.x = 0;
-  cpaint.y = 0;
+    
+  // Canvas for size selector
+  cpaint.canvas2 = $('#canvas2')[0];
+  cpaint.canvas2.width = $(cpaint.canvas2).width();
+  cpaint.canvas2.height = $(cpaint.canvas2).height();
+  cpaint.cx2 = cpaint.canvas2.getContext('2d');
+  $('#widthSlider').bind('change input', cpaint.widthSlider).trigger('change');
+    
+    
+  // Filters
+  $('#blur').bind('click', cpaint.blur);
+  $('#sharpen').bind('click', cpaint.sharpen);
+  $('#edgeDetect').bind('click', cpaint.edgeDetect);
+}
+
+cpaint.blur = function () {
+    cpaint.convolutionKernel(
+    [
+        [1,4,7,4,1],
+        [4,16,26,16,4],
+        [7,26,41,26,7],
+        [4,16,26,16,4],
+        [1,4,7,4,1],
+    ].map((row) => row.map((v) => v/271))
+    );
+}
+
+cpaint.sharpen = function () {
+    cpaint.convolutionKernel(
+    [
+        [-2, -2, -2],
+        [-2, 18, -2],
+        [-2, -2, -2],
+    ].map(row => row.map(v => v/1))
+    );
+}
+
+cpaint.edgeDetect = function () {
+    cpaint.convolutionKernel(
+    [
+        [1, 2, 1],
+        [0, 0, 0],
+        [-1, -2, -1],
+    ]
+    );
+}
+
+cpaint.convolutionKernel = function(convMatrix) { // kernels must be odd
+    console.log(convMatrix);
+    let imageData = cpaint.cx.getImageData(0,0, cpaint.canvas.width, cpaint.canvas.height);
+    let newImageData = cpaint.cx.createImageData(imageData.width, imageData.height);
+    
+    let getPixelComponentIndex = function(x,y,component) {
+        return component+x*4+y*newImageData.width*4;
+    }
+    
+    for (let y=0; y < newImageData.height; y++) {
+        for (let x=0; x < newImageData.width; x++) {
+            // Set opaque
+            newImageData.data[getPixelComponentIndex(x, y, 3)] = 255;
+            for (let c=0; c < 3; c++) { // r g b a
+                let acc = 0;
+                for (let mx=0; mx < convMatrix[0].length; mx++) {
+                    for (let my=0; my <convMatrix.length; my++) {
+                        let sourceX = -(convMatrix[0].length-1)/2 + x + mx;
+                        let sourceY = -(convMatrix.length-1)/2 + y + my;
+                        //console.log(sourceX, sourceY);
+                        sourceX = Math.min(Math.max(0, sourceX), newImageData.width-1);
+                        sourceY = Math.min(Math.max(0, sourceY), newImageData.height-1);
+                        let pxIndex = getPixelComponentIndex(sourceX, sourceY, c);
+                        //console.log(sourceX, sourceY, pxIndex, imageData.data[pxIndex]);
+                        acc += convMatrix[my][mx] * imageData.data[pxIndex];
+                    }
+                }
+                let newPxIndex = getPixelComponentIndex(x, y, c);
+                //console.log(c, acc, Math.min(Math.max(acc|0,0), 255));
+                newImageData.data[newPxIndex] = Math.min(Math.max(acc|0,0), 255);
+            }
+        }
+    }
+    cpaint.imgData = newImageData;
+    cpaint.cx.putImageData(cpaint.imgData, 0, 0);
+}
+
+
+
+cpaint.widthSlider = function(ev) {
+    cpaint.lineThickness = this.value;
+    let spotPos = cpaint.canvas2.width/2;
+    cpaint.cx2.strokeStyle = '#000000';
+    cpaint.cx2.clearRect(0, 0, cpaint.canvas.width, cpaint.canvas.height);
+    
+    cpaint.cx2.lineWidth = cpaint.lineThickness;
+    cpaint.drawLine(spotPos,spotPos,spotPos,spotPos, cpaint.cx2);
 }
 
 
@@ -129,12 +220,12 @@ cpaint.draw = function(ev) {
   cpaint.oldY = cpaint.y;
 } 
 
-cpaint.drawLine = function (beginX, beginY, endX, endY) {
-    cpaint.cx.beginPath();			// draw initial stroke
-    cpaint.cx.moveTo(beginX,beginY);
-    cpaint.cx.lineTo(endX,endY);
-    cpaint.cx.lineCap = 'round';
-    cpaint.cx.stroke();
+cpaint.drawLine = function (beginX, beginY, endX, endY, cx=cpaint.cx) {
+    cx.beginPath();			// draw initial stroke
+    cx.moveTo(beginX,beginY);
+    cx.lineTo(endX,endY);
+    cx.lineCap = 'round';
+    cx.stroke();
 }
 
 
@@ -193,7 +284,7 @@ cpaint.rect = function() {
     cpaint.newObject.endPos = {x: cpaint.x, y: cpaint.y};
     cpaint.cx.putImageData(cpaint.imgData, 0, 0);
     
-    console.log(cpaint.newObject);
+    //console.log(cpaint.newObject);
     cpaint.cx.beginPath();
     cpaint.cx.rect(cpaint.newObject.startPos.x, cpaint.newObject.startPos.y, 
                    cpaint.newObject.endPos.x - cpaint.newObject.startPos.x ,
